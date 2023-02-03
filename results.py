627dc72cd4f7b22f471217d0c63f93fa30f92273
NicholasSparks01 <nicholasgsparks@gmail.com>
2023-02-03 13:48:25 -0500

    cleaned up test_scans. Added results.py

---
+++ results.py
@@ -0,0 +1,4 @@
+from test_scans import active_hosts
+
+print(type(active_hosts))
+print(active_hosts)
--- test_scans.py
+++ test_scans.py
@@ -4,31 +4,25 @@ import random
 
 # Initialize the nmap scanner
 nm = nmap.PortScanner()
+active_hosts = []
 
 # Scan the local network for open ports
 nm.scan(hosts='192.168.1.0/24', arguments='-p 8008, 80, 8008, 8009, 8443, 9000, 10001, 5353, 53')
 # Iterate over the scanned hosts
 for host in nm.all_hosts():
-    print(f"Host: {host}")
+	print(f"Host: {host}")
 
-    # Check if the host is up
-    if nm[host].state() == "up":
-        print("  State: Up")
-	# creates array of all active hosts
-        active_hosts= active_hosts.append(nm[host])
+	# Check if the host is up
+	if nm[host].state() == "up":
+		print("  State: Up")
+		# creates array of all active hosts
+		active_hosts.append(nm[host])
 
-        # Iterate over the open ports on the host
-        # Added udp in nm[host][]
-        for port in nm[host]['tcp']:
-            print(f"  Port: {port}")
-            print(f"  State: {nm[host]['tcp'][port]['state']}")
+	# Iterate over the open ports on the host
+	# Added udp in nm[host][]
+	for port in nm[host]['tcp']:
+		print(f"  Port: {port}")
+		print(f"  State: {nm[host]['tcp'][port]['state']}")
 
-
-#print(nm.csv())
-#host;protocol;port;name;state;product;extrainfo;reason;version;conf
-
-# Exporting results to .csv. Maybe works???
-#nm[host] = str(nm[host])
-#with open('nm.csv()', 'w') as f:
-#    writer = csv.writer(f)
-#    writer.writerow(nm)
+#TESTING for import variable
+active_hosts= ['TEST']
