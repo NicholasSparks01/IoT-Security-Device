782d0c93765b80071c76c6f11af920218ad27ee1
NicholasSparks01 <nicholasgsparks@gmail.com>
2023-02-06 16:03:41 -0500

    added a hostname function

--- test_scans.py
+++ test_scans.py
@@ -7,22 +7,22 @@ nm = nmap.PortScanner()
 active_hosts = []
 
 # Scan the local network for open ports
-nm.scan(hosts='192.168.1.0/24', arguments='-p 8008, 80, 8008, 8009, 8443, 9000, 10001, 5353, 53')
+nm.scan(hosts='192.168.1.0/24', arguments='-p 8008, 80, 8008, 8883, 8009, 8443, 9000, 10001, 5353, 53')
 # Iterate over the scanned hosts
 for host in nm.all_hosts():
-	print(f"Host: {host}")
+        print(f"Host: %s (%s)" % (host, nm[host].hostname()))
 
-	# Check if the host is up
-	if nm[host].state() == "up":
-		print("  State: Up")
-		# creates array of all active hosts
-		active_hosts.append(nm[host])
+        # Check if the host is up
+        if nm[host].state() == "up":
+                print("  State: Up")
+                # creates array of all active hosts
+                active_hosts.append(nm[host])
 
-	# Iterate over the open ports on the host
-	# Added udp in nm[host][]
-	for port in nm[host]['tcp']:
-		print(f"  Port: {port}")
-		print(f"  State: {nm[host]['tcp'][port]['state']}")
+        # Iterate over the open ports on the host
+        # Added udp in nm[host][]
+        for port in nm[host]['tcp']:
+                print(f"  Port: {port}")
+                print(f"  State: {nm[host]['tcp'][port]['state']}")
 
 #TESTING for import variable
 active_hosts= ['TEST']
