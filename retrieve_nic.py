8350eec0d5a4e1de1b57e4192b6a37bb7210d4a7
NicholasSparks01 <93989654+NicholasSparks01@users.noreply.github.com>
2023-03-03 16:48:36 -0500

    possible new direction? is this needed?

--- retrieve_nic.py
+++ retrieve_nic.py
@@ -1,33 +1,19 @@
-import platform
-import subprocess
-import wmi
+import ifaddr
 
-def get_wifi_interface_windows():
-    c = wmi.WMI()
-    interfaces = c.Win32_NetworkAdapterConfiguration(IPEnabled=True)
-    for interface in interfaces:
-        if interface.Description.startswith("Intel(R) Dual Band Wireless"):
-            return interface.Description
-    return None
+wifi_interface_name = None
 
-def get_wifi_interface_linux():
-    try:
-        output = subprocess.check_output(["iwconfig"]).decode("utf-8").strip()
-        interfaces = output.split("\n")
-        for interface in interfaces:
-            if "wlp" in interface:
-                return interface.split()[0]
-    except subprocess.CalledProcessError:
-        return None
+# Get a list of all network interfaces on the system
+adapters = ifaddr.get_adapters()
 
-def get_wifi_interface():
-    if platform.system() == "Windows":
-        return get_wifi_interface_windows()
-    else:
-        return get_wifi_interface_linux()
+# Loop through the adapters and find the WiFi interface
+for adapter in adapters:
+    # if adapter.ips and "wlan" in adapter.nice_name:
+    #     wifi_interface_name = adapter.nice_name
+    #     break
+    print(adapter)
 
-wifi_interface = get_wifi_interface()
-if wifi_interface:
-    print("Wi-Fi interface:", wifi_interface)
+# Print the name of the WiFi interface, if it was found
+if wifi_interface_name:
+    print(f"WiFi interface name: {wifi_interface_name}")
 else:
-    print("No Wi-Fi interface found")
+    print("No WiFi interface found")
