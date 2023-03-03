2da5074f149acb8b8a9312a49750c8cd506f54fd
NicholasSparks01 <93989654+NicholasSparks01@users.noreply.github.com>
2023-03-03 16:23:19 -0500

    Pyshark single file

--- Pyshark/pyshark_tests.py
+++ Pyshark/pyshark_tests.py
@@ -3,6 +3,24 @@
 # Created by: Nicholas Sparks
 import pyshark
 
-capture = pyshark.LiveCapture(interface='Wi-Fi', output_file='Test.cap')
-capture.sniff(timeout=20)
+# capture = pyshark.LiveCapture(interface='Wi-Fi', output_file='Test.cap')
+# capture.sniff(timeout=20)
+PossibleIP = []
+WiresharkFilter = 'udp.port == 5353 and mdns contains "Google" or mdns contains "Amazon" or mdns contains "amazon" or mdns contains "google"'
 
+def Live_Capture():
+    capture = pyshark.LiveCapture(interface='Wi-Fi', output_file='Test.cap')
+    capture.sniff(timeout=20)
+
+def Packet_Analysis():
+    for packet in pyshark.FileCapture('Test.cap', display_filter= WiresharkFilter):
+        source_address = packet.ip.src
+        # print(type(source_address))
+        if source_address not in PossibleIP:
+            PossibleIP.append(str(source_address))
+    return PossibleIP
+
+
+Live_Capture()
+list = Packet_Analysis()
+print(list)
